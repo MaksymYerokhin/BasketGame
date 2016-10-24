fc57d917614a08096e13767baebe6d7258b5cf5b
Maxim Yerokhin <Maxim Yerokhin@DESKTOP-ROI1VF1>
2016-10-24 14:32:39 +0300

    15

--- BasketGame.Demo/Program.cs
+++ BasketGame.Demo/Program.cs
@@ -6,27 +6,97 @@ namespace BasketGame.Demo
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private const int MinPlayers = 2;
+
+        private const int MaxPlayers = 8;
+
+        private const int MinWeight = 40;
+
+        private const int MaxWeight = 140;
+
+        private const int MaxAttempts = 100;
+
+        private const int MaxMilliseconds = 1500;
+
+        private static void Main()
         {
             var game = new Game();
-            Console.WriteLine("Players types:\n 0 - Thorough\n 1 - Memory\n 2 - Random\n 3 - Cheater\n 4 - Thorough Cheater\n");
-            Console.Write("Enter players number: ");
-            var playersNumber = Convert.ToInt32(Console.ReadLine());
+            var restriction = new GameRestriction(MinPlayers, MaxPlayers, MinWeight, MaxWeight, MaxAttempts, MaxMilliseconds);
+            ShowPlayerTypes();
+            var playersNumber = ReadPlayersNumber(restriction);
+            var players = ReadPlayersInfo(playersNumber);
 
+            game.Initialize(players, restriction);
+            Console.WriteLine(game.Announcer.AnnounceInitialData());
+            var result = game.Play();
+            Console.WriteLine(result);
+        }
+
+        private static void ShowPlayerTypes()
+        {
+            Console.WriteLine("Available player types numbers:");
+            var playerTypes = Enum.GetValues(typeof(PlayerType));
+            for (var i = 0; i < playerTypes.Length; i++)
+            {
+                Console.WriteLine($"{i} - {playerTypes.GetValue(i)}");
+            }
+            Console.WriteLine("\n");
+        }
+
+        private static int ReadPlayersNumber(GameRestriction restriction)
+        {
+            bool isValid;
+            int result;
+
+            do
+            {
+                Console.Write("Enter players number: ");
+                var input = Console.ReadLine();
+                isValid = int.TryParse(input, out result);
+                isValid = isValid && (result <= restriction.MaxPlayers &&
+                                      result >= restriction.MinPlayers);
+
+                if (!isValid)
+                {
+                    Console.WriteLine($"Type correct integer number between {restriction.MinPlayers} and {restriction.MaxPlayers}.");
+                }
+            } while (!isValid);
+            
+            return result;
+        }
+
+        private static List<PlayerInfo> ReadPlayersInfo(int playersNumber)
+        {
             var players = new List<PlayerInfo>(playersNumber);
             for (var i = 0; i < playersNumber; i++)
             {
                 Console.Write($"Enter Player {i + 1} name: ");
                 var name = Console.ReadLine();
-                Console.Write($"Enter Player {i + 1} type: ");
-                var type = (PlayerType)Convert.ToInt32(Console.ReadLine());
-                players.Add(new PlayerInfo(name, type));
-            }
+                var inputType = ReadPlayerType(i);
 
-            game.Initialize(players, new GameRestriction(2, 8, 40, 140, 100, 1500));
-            Console.WriteLine(game.Announcer.AnnounceInitialData());
-            var result = game.Play();
-            Console.WriteLine(result);
+                players.Add(new PlayerInfo(name, inputType));
+            }
+            return players;
+        }
+
+        private static PlayerType ReadPlayerType(int i)
+        {
+            bool isValid;
+            int resultInt;
+
+            do
+            {
+                Console.Write($"Enter Player {i + 1} type: ");
+                var inputType = Console.ReadLine();
+                isValid = int.TryParse(inputType, out resultInt);
+                isValid = isValid && Enum.IsDefined(typeof(PlayerType), resultInt);
+                if (!isValid)
+                {
+                    Console.WriteLine("Select one of the Player Type numbers provided at the top.");
+                }
+            } while (!isValid);
+            
+            return (PlayerType)resultInt;
         }
     }
 }
